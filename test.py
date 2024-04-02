7a9b908fff98289b5542ebc176b591275118b628
StefanoWind <stefano.letizia@nrel.gov>
2024-04-02 15:13:32 -0600

    Updated test.py

--- test.py
+++ test.py
@@ -10,10 +10,10 @@ import LIDARGO_statistics as lc0
 
 files=glob.glob('data/**/*a0*nc')
 for f in files:
-    lproc = lb0.LIDARGO(f,'data/configs_standardize.xlsx', verbose=True)
+    lproc = lb0.LIDARGO(f,'config/configs_standardize.xlsx', verbose=True)
     lproc.process_scan(f,replace=True,save_file=True)
 
 files=glob.glob('data/**/*b0*nc')
 for f in files:
-    lproc = lc0.LIDARGO(f,'data/configs_statistics.xlsx', verbose=True)
+    lproc = lc0.LIDARGO(f,'config/configs_statistics.xlsx', verbose=True)
     lproc.process_scan(f,replace=True,save_file=True)
