b06de52a8e60a2e8cd0278f89ce7e60798327967
StefanoWind <stefano.letizia@nrel.gov>
2024-10-25 17:37:24 -0600

    finalized example4

--- examples/Example4.py
+++ examples/Example4.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 '''
-Example of application of LiDARGO to AWAKEN stare data from sa5.lidar.z03 data channel and wind spectra.
+Example of application of LiDARGO to AWAKEN stare data from sa5.lidar.z03 data channel and radial velocity spectra.
 '''
 
 import os
@@ -14,7 +14,10 @@ from matplotlib import pyplot as plt
 import warnings
 import matplotlib
 import numpy as np
-from scipy.signal import welch
+from scipy import signal
+
+matplotlib.rcParams['font.family'] = 'serif'
+matplotlib.rcParams['mathtext.fontset'] = 'cm' 
 
 warnings.filterwarnings('ignore')
 plt.close('all')
@@ -26,8 +29,6 @@ filename1=os.path.join(pwd,'data/Example4/sa5.lidar.z03.a0.20231009.205005.user5
 filename2=os.path.join(pwd,'data/Example4/sa5.lidar.z03.b0.20231009.205005.user5.awaken.stare.nc')
 source_config_stand=os.path.join(pwd,'config/config_examples_stand.xlsx')
 
-welch_window=50#number of FFT point in Welch
-
 #%% Main
 
 #standardization
@@ -36,31 +37,31 @@ lproc.process_scan(filename1,replace=True,save_file=True)
 
 #read standardized data
 Data=xr.open_dataset(filename2)
-
 rws=np.squeeze(Data['wind_speed'].where(Data['qc_wind_speed']==0).values)
-fs=(np.float64(Data.time.diff(dim='scanID').mean('scanID').values)/10**9)**-1
 r=Data.range.values
 
-#calculate Welch power spectral density
-psd=np.zeros((len(r),int(np.ceil(welch_window/2))))+np.nan
+fs=(np.float64(Data.time.diff(dim='scanID').mean('scanID').values)/10**9)**-1#sampling frequency
+
+#calculate power spectral density
+psd=np.zeros((len(r),int(len(Data.scanID)/2+1)))+np.nan
 for i in range(len(r)):
-    f, psd[i,:] = welch(rws[i,:]-np.nanmean(rws[i,:]), fs, nperseg=welch_window)
+    f, psd[i,:] = signal.periodogram(rws[i,:]-np.nanmean(rws[i,:]), fs)
     
 #%% Plots
 plt.close('all')
-matplotlib.rcParams['font.family'] = 'serif'
-matplotlib.rcParams['mathtext.fontset'] = 'cm' 
-matplotlib.rcParams['font.size'] = 12
+matplotlib.rcParams['font.size'] = 13
 
 plt.figure(figsize=(18,8))
-sel= np.where(np.sum(~np.isnan(psd),axis=0)>0)[0]
+sel= np.where(np.sum(~np.isnan(psd),axis=1)>0)[0]
 cmap = plt.cm.get_cmap('viridis')
 colors = [cmap(i / (len(sel) - 1)) for i in range(len(sel))]
 for s,c in zip(sel,colors):
-    plt.loglog(f,psd[s,:],label=r'$r='+str(r[s])+'$m',color=c)
-
+    plt.loglog(f,psd[s,:],label=r'$r='+str(r[s])+'$ m',color=c)
+plt.ylim([10**-6,10**3])
 plt.xlabel(r'Frequency [Hz]')
-plt.ylabel('Power spectral density [m$^2$ s$^{-1}$]')
+plt.ylabel(r'Power spectral density of $u^\prime_{LOS}$ [m$^2$ s$^{-1}$]')
 plt.grid()
 plt.legend()
+
+
 
