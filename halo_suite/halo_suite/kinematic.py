f701bf730c14e99c29b32e991588ee7bb71b97cc
StefanoWind <ltzsfn@gmail.com>
2025-09-29 12:36:20 -0600

    SSM kinematic done

--- halo_suite/halo_suite/kinematic.py
+++ halo_suite/halo_suite/kinematic.py
@@ -7,20 +7,128 @@ cd=os.path.dirname(__file__)
 from matplotlib import pyplot as plt
 import numpy as np
 import yaml
-from halo_suite.utilities import scan_file_compiler
+from halo_suite.utilities import scan_file_compiler, read_hpl
+from scipy.optimize import curve_fit
+import glob
+plt.close('all')
 
 #%% Inputs
 path_config=os.path.join(cd,'configs/config.yaml')
+lidar_id=input('Lidar ID: ')
+mode=input('Mode (SSM or CSM): ')
 
 #%% Initialization
 #configs
 with open(path_config, 'r') as fid:
     config = yaml.safe_load(fid)      
 
+def kinematic_model(dang,S,A):
+    dt=dang+np.nan
+    ang_crit=S**2/A
+    dt[dang<ang_crit]=2*(dang[dang<ang_crit]/A)**0.5
+    dt[dang>=ang_crit]=dang[dang>=ang_crit]/S+S/A
+    
+    return dt
+    
 #%% Main
 
-#write file for sampling time test
-scan_file_compiler(mode='SSM',azi=90,ele=90,repeats=config['repeats_test'])
-print(f'Test stare file saved. Run it on the lidar with different PPRs.')
+#static acquisition
+files=glob.glob(os.path.join(config['path_data'],f'{lidar_id}','stares.kinematic',f'{mode}','*hpl'))
+if len(files)==0:
+    scan_file_compiler(mode=mode,azi=90,ele=90,repeats=config['repeats_test'],
+                       identifier=f'Stare_{config["repeats_test"]}_points',config=config,lidar_id=lidar_id)
+    print(f'Test stare file saved. Run it on the lidar with different PPRs. Save data in ./data/{lidar_id}/stares_kinematic')
+else:
+    dt_avg=[]
+    dt_low=[]
+    dt_top=[]
+    ppr_all=[]
+    for f in files:
+        tnum,azi,ele,Nr,dr,ppr=read_hpl(f,lidar_id,config)
+        dt_avg=np.append(dt_avg,np.nanmedian(np.diff(tnum)))
+        dt_low=np.append(dt_low,np.nanpercentile(np.diff(tnum),5))
+        dt_top=np.append(dt_top,np.nanpercentile(np.diff(tnum),95))
+        ppr_all=np.append(ppr_all,ppr)
+        
+    LF=np.polyfit(ppr_all,dt_avg,1)
+    dt_d=np.round(LF[1],4)
+    dt_dppr=np.round(LF[0]*1000,4)/1000
+    
+    output={'dt_d':dt_d,
+            'dt_dppr':dt_dppr}
+    
+    #plot linear fit
+    plt.figure()
+    plt.plot(ppr_all,dt_avg,'.k',markersize=10,label='Stare data')
+    plt.errorbar(ppr_all,dt_avg,[dt_avg-dt_low,dt_top-dt_avg],fmt='o',color='k',capsize=5)
+    plt.plot(ppr_all,dt_d+dt_dppr*ppr_all,'-r',label=r'$'+str(dt_d)+'+'+str(dt_dppr*1000)+r'\cdot 10^{-3}$ PPR',zorder=10)
+    plt.xlabel('PPR')
+    plt.ylabel(r'$\Delta t$ [s]')
+    plt.title(f'{mode}')
+    plt.grid()
+    plt.legend()
+    
+#dynamic acquisition
+files=glob.glob(os.path.join(config['path_data'],f'{lidar_id}','ppi_rhi.kinematic','*hpl'))
+if len(files)==0:
+    azi=[]
+    ele=[]
+    for dazi in config['dazi_test']:
+        max_azi=config['azi_max_test']
+        if dazi>=max_azi/2:
+            max_azi=360
+        new_azi=np.arange(0,max_azi,dazi+0.1)
+        azi=np.append(azi,new_azi)
+        ele=np.append(ele,new_azi*0)
+        
+    for dele in config['dele_test']:
+        max_ele=config['ele_max_test']
+        if dele>=max_ele/2:
+            max_ele=180
+        new_ele=np.arange(0,max_ele,dele+0.1)
+        azi=np.append(azi,new_ele*0)
+        ele=np.append(ele,new_ele)
+        
+    scan_file_compiler(mode=mode,azi=azi,ele=ele,repeats=1,identifier=f'PPI_RHI_{len(azi)}_points')
+    print(f'Test PPI+RHI file saved. Run it on the lidar with different PPRs. Save data in ./data/{lidar_id}/ppi_rhi.kinematic')
+else:
+    for f in files:
+        tnum,azi,ele,Nr,dr,ppr=read_hpl(f,lidar_id,config)
+        dt_m=np.diff(tnum)
+        dazi=((azi[1:] - azi[:-1] + 180) % 360) - 180
+        dele=((ele[1:] - ele[:-1] + 180) % 360) - 180
+        
+        #fit kinematic model
+        V, cov = curve_fit(kinematic_model,dazi[dazi>0.01],dt_m[dazi>0.01]-dt_d-dt_dppr*ppr)
+        S_azi=np.round(V[0],1)
+        A_azi=np.round(V[1],1)
+        
+        V, cov = curve_fit(kinematic_model,dele[dele>0.01],dt_m[dele>0.01]-dt_d-dt_dppr*ppr)
+        S_ele=np.round(V[0],1)
+        A_ele=np.round(V[1],1)
+        
+        plt.figure(figsize=(16,8))
+        dazi_plot=np.arange(0,np.nanmax(dazi),0.1)
+        dele_plot=np.arange(0,np.nanmax(dele),0.1)
+        ax=plt.subplot(2,1,1)
+        plt.plot(dazi[dazi>0.01],dt_m[dazi>0.01]-dt_d-dt_dppr*ppr,'.k',alpha=0.5,label='PPI data')
+        plt.plot(dazi_plot,kinematic_model(dazi_plot,S_azi, A_azi),'-r',
+                 label=r'$\dot{\alpha}_{max}='+str(S_azi)+r'^\circ s^{-1}$'+'\n'+r'$\ddot{\alpha}='+str(A_azi)+r'^\circ s^{-2}$')
+        plt.xlabel(r'$\Delta \alpha$ [$^\circ$]')
+        plt.ylabel(r'$\Delta t_m$ [s]')
+        plt.legend()
+        plt.grid()
+        
+        plt.title(f'PPR={ppr}, mode={mode}, file: {os.path.basename(f)}')
+        ax=plt.subplot(2,1,2)
+        plt.plot(dele[dele>0.01],dt_m[dele>0.01]-dt_d-dt_dppr*ppr,'.k',alpha=0.5,label='RHI data')
+        plt.plot(dele_plot,kinematic_model(dele_plot,S_ele, A_ele),'-r',
+                 label=r'$\dot{\beta}_{max}='+str(S_ele)+r'^\circ s^{-1}$'+'\n'+r'$\ddot{\beta}='+str(A_ele)+r'^\circ s^{-2}$')
+        plt.xlabel(r'$\Delta \beta$ [$^\circ$]')
+        plt.ylabel(r'$\Delta t_m$ [s]')
+        plt.legend()
+        plt.grid()
+    
+
     
 
--- halo_suite/halo_suite/utilities.py
+++ halo_suite/halo_suite/utilities.py
@@ -16,37 +16,107 @@ def scan_file_compiler(mode: str,
                        save_path: str='',
                        identifier: str='',
                        volumetric: bool=False,
-                       model: str='',
-                       ppr: int=0):
+                       lidar_id: str='',
+                       ppr: int=0,
+                       S_azi: float=36,
+                       S_ele: float=72,
+                       config: dict={},
+                       kinematic_file: str=''):
     
     if save_path=='':
         save_path=os.path.join(cd,'scans')
     if identifier=='':
         identifier=datetime.strftime(datetime.now(),'%Y%m%d.%H%M%S')
     
-    if mode=='SSM':
+    if volumetric:
+        [azi,ele]=np.meshgrid(azi,ele)
+        azi=azi.ravel()
+        ele=ele.ravel()
+        vol_flag='vol.'
+    else:
+        vol_flag=''
         
-        if volumetric:
-            [azi,ele]=np.meshgrid(azi,ele)
-            azi=azi.ravel()
-            ele=ele.ravel()
-            vol_flag='vol.'
-        else:
-            vol_flag=''
-            
+    if mode=='SSM':
         if isinstance(azi, np.ndarray):
             L=''
             for a,e in zip(azi[:-1],ele[:-1]):
                 L=L+('%07.3f' % a+ '%07.3f' % e +'\n')
         else:
             L='%07.3f' % azi+ '%07.3f' % ele +'\n'
-            
-            
-        save_name=f'{identifier}.{mode}.{vol_flag}.txt'
     
+    if mode =='CSM':
+        if type(azi).__name__!= 'ndarray':
+            if type(azi).__name__!='list':
+                azi=[azi]
+            azi=np.array(azi)
+        if type(ele).__name__!= 'ndarray':
+            if type(ele).__name__!='list':
+                ele=[ele]
+            ele=np.array(ele)
+        azi=np.append(azi,azi[0])
+        ele=np.append(ele,ele[0])
+        if kinematic_file=='':
+            ppd1=config['ppd_azi'][lidar_id]#points per degree in azimuth motor
+            ppd2=config['ppd_ele'][lidar_id]#points per degree in elevation motor
+            
+            dazi=((azi[1:] - azi[:-1] + 180) % 360) - 180
+            dele=((ele[1:] - ele[:-1] + 180) % 360) - 180
+            stop=np.concatenate(([0],np.where(np.abs((np.diff(dazi)+np.diff(dele)))>10**-10)[0]+1,[-1]))
+            
+            azi_range=azi[stop]
+            P1=-azi_range*ppd1
+            S1=S_azi*ppd1/10+np.zeros(len(P1))
+            A1=config['max_A_azi'][lidar_id]+np.zeros(len(P1))
+            a1=A1*1000/ppd1
+            
+            ele_range=ele[stop]
+            P2=-ele_range*ppd2
+            S2=S_ele*ppd1/10+np.zeros(len(P1))
+            A2=config['max_A_ele'][lidar_id]+np.zeros(len(P2))
+            a2=A2*1000/ppd1
+        L=''
+        for p1,p2,s1,s2,a1,a2 in zip(P1[:-1],P2[:-1],S1[:-1],S2[:-1],A1[:-1],A2[:-1]):
+            
+            l='A.1=%.0f'%a1+',S.1=%.0f'%s1+',P.1=%.0f'%p1+'*A.2=%.0f'%a2+',S.2=%.0f'%s2+',P.2=%.0f'%p2+'\nW=0\n'
+            L+=l
+        
     os.makedirs(save_path,exist_ok=True)
+    save_name=f'{identifier}.{mode}.{vol_flag}.txt'
     with open(os.path.join(save_path,save_name),'w') as fid:
         fid.write(L*repeats)
         fid.close()
+        
+        
+def read_hpl(file,lidar_id,config):
+    '''
+    Read kinematic from hlp lidar file
+    '''
+    
+    #read ranges
+    with open(file, "r") as fid:
+        Nr=int(fid.readlines()[config['hpl_Nr']['217']].split(':')[1].strip())
+        fid.seek(0)
+        dr=np.float64(fid.readlines()[config['hpl_dr']['217']].split(':')[1].strip())
+        fid.seek(0)
+        ppr=int(fid.readlines()[config['hpl_ppr']['217']].split(':')[1].strip())
+        fid.seek(0)
+        data=fid.readlines()[config['hpl_header'][lidar_id]::Nr+1]
+
+        #zeroing (specific file)
+        tnum=[]
+        azi=[]
+        ele=[]
+         
+        #read only first gate info (time, angles)
+        for d in np.array(data):
+            d_split=np.array(d.split(' '))
+            d_split=d_split[d_split!='']
+            tnum=np.append(tnum,np.float64(d_split[config['hpl_index_time'][lidar_id]])*3600)
+            azi=np.append(azi,np.float64(d_split[config['hpl_index_azi'][lidar_id]]))
+            ele=np.append(ele,np.float64(d_split[config['hpl_index_ele'][lidar_id]]))
+                
+        return tnum, azi, ele, Nr, dr, ppr
+            
+
         
                        
