aa64eaf9cf483a5b1a98643ea3b6403f4a6cf3f6
StefanoWind <ltzsfn@gmail.com>
2025-11-06 15:06:42 -0700

    pareto plots in utilities

--- lisboa/lisboa/scan_optimizer.py
+++ lisboa/lisboa/scan_optimizer.py
@@ -19,15 +19,17 @@ class scan_optimizer:
                  config: dict,
                  save_path: str=os.path.join(cd,'data','Pareto'),
                  save_data: bool=True,
+                 make_figures: bool=True,
                  save_figures: bool=True,
                  identifier: str=''):
         
         if identifier!='':
             identifier='.'+identifier
         self.config=config
+        self.make_figures=make_figures
         self.save_figures=save_figures
         if save_data:
-            self.save_name=os.path.join(save_path,datetime.strftime(datetime.now(),'%Y%m%d.%H%M')+identifier)
+            self.save_name=os.path.join(save_path,datetime.strftime(datetime.now(),'%Y%m%d.%H%M%S')+identifier)
             os.makedirs(self.save_name)
       
     def pareto(
@@ -52,12 +54,7 @@ class scan_optimizer:
         #zeroing
         epsilon1=np.zeros((len(azi1),len(dazi)))+np.nan
         epsilon2=np.zeros((len(azi1),len(dazi)))+np.nan
-        grid=np.empty((len(azi1),len(dazi)),dtype=object)
-        Dd=np.empty((len(azi1),len(dazi)),dtype=object)
-        excl=np.empty((len(azi1),len(dazi)),dtype=object)
-        points=np.empty((len(azi1),len(dazi)),dtype=object)
         duration=np.zeros((len(azi1),len(dazi)))+np.nan
-        scan_file=np.empty((len(azi1),len(dazi)),dtype=object)
         
         #initialize LiSBOA
         lproc=stats.statistics(self.config)
@@ -65,7 +62,7 @@ class scan_optimizer:
         with open(path_config_lidar, 'r') as fid:
             config_lidar = yaml.safe_load(fid)    
         
-        r=np.arange(rmin,rmax+dr,dr)
+        r=np.arange(rmin,rmax+dr/2,dr)
         i_ang=0
         for a1,a2,e1,e2 in zip(azi1,azi2,ele1,ele2):
             i_dang=0
@@ -120,7 +117,7 @@ class scan_optimizer:
                 elif coords=='xyz':
                     x_exp=[x.ravel(),y.ravel(),z.ravel()]
                 grid,Dd,excl,_,_,_,_=lproc.calculate_weights(x_exp)
-                epsilon1[i_ang,i_dang]=np.sum(excl[i_ang,i_dang])/np.size(excl[i_ang,i_dang])
+                epsilon1[i_ang,i_dang]=np.sum(excl)/np.size(excl)
                 
                 #epsilon2
                 L=np.floor(T/t[-1])
@@ -148,11 +145,11 @@ class scan_optimizer:
                                         attrs={'description':'y of experimental points'})
                     Output['z_points']=xr.DataArray(z.ravel(),coords={'index':np.arange(len(z.ravel()))},
                                         attrs={'description':'z of experimental points'})
-                    Output['azi']=xr.DataArray(azi,coords={'index_azi':len(azi)},
+                    Output['azi']=xr.DataArray(azi,coords={'index_azi':np.arange(len(azi))},
                                                attrs={'description':'azimuth angle','units':'degrees'})
-                    Output['ele']=xr.DataArray(azi,coords={'index_ele':len(ele)},
+                    Output['ele']=xr.DataArray(ele,coords={'index_ele':np.arange(len(ele))},
                                                attrs={'description':'elevation angle','units':'degrees'})
-                    Output['r']=xr.DataArray(azi,coords={'index_r':len(r)},
+                    Output['r']=xr.DataArray(r,coords={'index_r':np.arange(len(r))},
                                                attrs={'description':'range','units':'m'})
                     
                     Output.attrs={}
@@ -163,10 +160,13 @@ class scan_optimizer:
                     Output.attrs['epsilon1']=epsilon1[i_ang,i_dang]
                     Output.attrs['epsilon2']=epsilon2[i_ang,i_dang]
                     Output.attrs['duration']=duration[i_ang,i_dang]
+                    Output.attrs['volumetric']=str(volumetric)
                     Output.to_netcdf(os.path.join(self.save_name,scan_name+'.nc'))
                     
+                if self.make_figures:
+                    fig=utl.visualize_scan(Output)
                 if self.save_figures:
-                    self.visualize_scan(Output)
+                    fig.savefig(os.path.join(self.save_name,scan_name+'.png'))
                     
                 i_dang+=1
             i_ang+=1
@@ -192,172 +192,12 @@ class scan_optimizer:
         Output['dele']=xr.DataArray(dele,coords={'index_dang':np.arange(len(dazi))},
                             attrs={'description':'end elevation','units':'degrees'})
         
+        Output.to_netcdf(os.path.join(self.save_name+'.Pareto.nc'))
         
-        Output.to_netcdf(os.path.join(self.save_name+'.nc'))
-        
-        return Output
-    
-    def visualize_scan(self,Data):
-        info=r'$\alpha='+str(Data.azi[0])+':'+str(Data.azi[-1])+':'+str(Data.ele.diff()[0])+'^\circ$'+ '\n'+\
-             r'$\beta=' +str(Data.ele[0])+':'+str(Data.ele[-1])+':'+str(Data.ele.diff()[0])+'^\circ$' + '\n'+\
-             r'$r= '    +str(Data.r[0])+':'+str(Data.r[-1])+':'+str(Data.r.diff()[0])+ '$ m' +'\n'+\
-             r'$\Delta n_0='+str(self.config['Dn0'])+r'$ m'                                         + '\n'+\
-             r'$\epsilon_I='+str(np.round(Data.attrs.epsilon1,2))+r'$'                      + '\n'+\
-             r'$\epsilon_{II}='+str(np.round(Data.attrs.epsilon2,2))+r'$'                   + '\n'+\
-             r'$\tau_s='+str(np.round(Data.attrs.duration,1))+r'$ s'
-         
-        coords=''
-        for c in Data.excl.coords:
-            coords+=c
-   
-        if coords!='xyz':
-            fill=np.zeros(np.shape(Data.excl))
-            fill[Data.excl==False]=10
-            ax=plt.subplot(111)
-            plt.pcolor(Data[coords[0]],Data[coords[1]],fill.T,cmap='Greys',vmin=0,vmax=1,alpha=0.5)
-            plt.plot(Data[coords[0]+'_points'],Data[coords[1]+'_points'],'.k',markersize=2)
-            ax.set_aspect('equal')
-            ax.set_xlim([self.config['mins'][0],self.config['maxs'][0]])
-            ax.set_ylim([self.config['mins'][1],self.config['maxs'][1]])
-            ax.set_xlabel(r'$'+str(coords[0])+'$ [m]')
-            ax.set_ylabel(r'$'+str(coords[1])+'$ [m]')
-            dtick=np.max([np.diff(ax.get_xticks())[0],
-                          np.diff(ax.get_yticks())[0]])
-            ax.set_xticks(np.arange(self.config['mins'][0],self.config['maxs'][0]+dtick,dtick))
-            ax.set_yticks(np.arange(self.config['mins'][1],self.config['maxs'][1]+dtick,dtick))
-            ax.text(0,self.config['maxs'][1]/2,s=info,bbox={'edgecolor':'k','facecolor':(1,1,1,0.25)})
-
-        elif coords=='xyz':
-            fill=excl[i_ang,i_dang]==False
-            ax=plt.subplot(N_row,N_col,i_dang+1,projection='3d')
-            dx=np.diff(grid[i_ang,i_dang][0])[0]
-            dy=np.diff(grid[i_ang,i_dang][1])[0]
-            dz=np.diff(grid[i_ang,i_dang][2])[0]
-            X,Y,Z=np.meshgrid(np.append(grid[i_ang,i_dang][0]-dx/2,grid[i_ang,i_dang][0][-1]+dx),
-                      np.append(grid[i_ang,i_dang][1]-dy/2,grid[i_ang,i_dang][1][-1]+dy),
-                      np.append(grid[i_ang,i_dang][2]-dz/2,grid[i_ang,i_dang][2][-1]+dz),indexing='ij')
-            ax.voxels(X,Y,Z,fill,facecolor=colors[i_dang],alpha=0.1)
-            sel_x=(points[i_ang,i_dang][0]>config['mins'][0])*(points[i_ang,i_dang][0]<config['maxs'][0])
-            sel_y=(points[i_ang,i_dang][1]>config['mins'][1])*(points[i_ang,i_dang][1]<config['maxs'][1])
-            sel_z=(points[i_ang,i_dang][2]>config['mins'][2])*(points[i_ang,i_dang][2]<config['maxs'][2])
-            sel=sel_x*sel_y*sel_z
-            plt.plot(points[i_ang,i_dang][0][sel],points[i_ang,i_dang][1][sel],points[i_ang,i_dang][2][sel],'.k',markersize=2)
-            ax.set_aspect('equal')
-            ax.set_xlim([config['mins'][0],config['maxs'][0]])
-            ax.set_ylim([config['mins'][1],config['maxs'][1]])
-            ax.set_zlim([config['mins'][2],config['maxs'][2]])
-            ax.set_xlabel(r'$x$ [m]')
-            ax.set_ylabel(r'$y$ [m]')
-            ax.set_zlabel(r'$z$ [m]')
-            dtick=np.max([np.diff(ax.get_xticks())[0],
-                          np.diff(ax.get_yticks())[0],
-                          np.diff(ax.get_zticks())[0]])
-            ax.set_xticks(np.arange(config['mins'][0],config['maxs'][0]+dtick,dtick))
-            ax.set_yticks(np.arange(config['mins'][1],config['maxs'][1]+dtick,dtick))
-            ax.set_zticks(np.arange(config['mins'][2],config['maxs'][2]+dtick,dtick))
-            ax.text(0,0,config['maxs'][2]/2,s=info,bbox={'edgecolor':'k','facecolor':(1,1,1,0.25)})
-          
-    def visualize_Pareto(self,Data):
-        
-        #extract information
-      
-        N_ang=len(Pareto.index_ang)
-        N_dang=len(Pareto.index_dang)
-        epsilon1=Pareto.epsilon1.values
-        epsilon2=Pareto.epsilon2.values
-        excl=Pareto.excl.values
-        duration=Pareto.duration.values
-        azi1=Pareto.azi1
-        azi2=Pareto.azi2
-        ele1=Pareto.ele1
-        ele2=Pareto.ele2
-        dazi=Pareto.dazi
-        dele=Pareto.dele
-        
-        #plot Pareto front
-        fig=plt.figure(figsize=(18,8))
-        cmap = plt.cm.jet
-        colors = [cmap(v) for v in np.linspace(0,1,len(Pareto.dazi))]
-        N_row=int(np.floor(N_ang**0.5))
-        N_col=int(np.ceil(N_ang/N_row))
-        for i_ang in range(N_ang):
-            ax=plt.subplot(N_row,N_col,i_ang+1)
-            plt.plot(epsilon1[np.arange(N_ang)!=i_ang,:],epsilon2[np.arange(N_ang)!=i_ang,:],'.k',markersize=30,alpha=0.25)
-            for i_dang in range(N_dang):
-                plt.plot(epsilon1[i_ang,i_dang],epsilon2[i_ang,i_dang],'.',
-                         color=colors[i_dang],markeredgecolor='k',markersize=30,
-                         label=r'$\Delta \alpha='+str(dazi[i_dang])+r'^\circ$, $\Delta \beta='+str(dele[i_dang])+r'^\circ$')
-            plt.xlim([0,1])
-            plt.ylim([0,1])
-            plt.xlabel(r'$\epsilon_I$')
-            plt.ylabel(r'$\epsilon_{II}$')  
-            plt.title(r'$\alpha \in ['+str(azi1[i_ang])+', '+str(azi2[i_ang])+r']^\circ$, $\beta \in ['+str(ele1[i_ang])+r', '+str(ele2[i_ang])+']^\circ$')
-            plt.grid()
-        plt.legend(draggable=True)
-
-        #plot all scan geometries
-        for i_ang in range(N_ang):
-            fig=plt.figure(figsize=(18,8))
-            N_row=int(np.floor(N_dang**0.5))
-            N_col=int(np.ceil(N_dang/N_row))
-            for i_dang in range(N_dang):  
-                info=r'$\alpha='+str(azi1[i_ang])+':'+str(dazi[i_dang])+':'+str(azi2[i_ang])+'^\circ$'+ '\n'+\
-                     r'$\beta=' +str(ele1[i_ang])+':'+str(dele[i_dang])+':'+str(ele2[i_ang])+'^\circ$' + '\n'+\
-                     r'$\Delta n_0='+str(self.config['Dn0'])+r'$ m'                                         + '\n'+\
-                     r'$\epsilon_I='+str(np.round(epsilon1[i_ang,i_dang],2))+r'$'                      + '\n'+\
-                     r'$\epsilon_{II}='+str(np.round(epsilon2[i_ang,i_dang],2))+r'$'                   + '\n'+\
-                     r'$\tau_s='+str(np.round(duration[i_ang,i_dang],1))+r'$ s'
+        if self.make_figures:
+            fig=utl.visualize_Pareto(Output)
+        if self.save_figures:
+            fig.savefig(os.path.join(scan_name+'Pareto.png'))
             
-                if coords!='xyz':
-                    fill=np.zeros(np.shape(excl[i_ang,i_dang]))
-                    fill[excl[i_ang,i_dang]==False]=10
-                    
-                    ax=plt.subplot(N_row,N_col,i_dang+1)
-                    plt.pcolor(grid[i_ang,i_dang][0],grid[i_ang,i_dang][1],fill.T,cmap='Greys',vmin=0,vmax=1,alpha=0.5)
-                    plt.plot(points[i_ang,i_dang][0],points[i_ang,i_dang][1],'.',color=colors[i_dang],markersize=2)
-                    ax.set_aspect('equal')
-                    ax.set_xlim([config['mins'][0],config['maxs'][0]])
-                    ax.set_ylim([config['mins'][1],config['maxs'][1]])
-                    ax.set_xlabel(r'$'+str(coords[0])+'$ [m]')
-                    ax.set_ylabel(r'$'+str(coords[1])+'$ [m]')
-                    dtick=np.max([np.diff(ax.get_xticks())[0],
-                                  np.diff(ax.get_yticks())[0]])
-                    ax.set_xticks(np.arange(config['mins'][0],config['maxs'][0]+dtick,dtick))
-                    ax.set_yticks(np.arange(config['mins'][1],config['maxs'][1]+dtick,dtick))
-                    ax.text(0,config['maxs'][1]/2,s=info,bbox={'edgecolor':'k','facecolor':(1,1,1,0.25)})
-
-                elif coords=='xyz':
-                    fill=excl[i_ang,i_dang]==False
-                    ax=plt.subplot(N_row,N_col,i_dang+1,projection='3d')
-                    dx=np.diff(grid[i_ang,i_dang][0])[0]
-                    dy=np.diff(grid[i_ang,i_dang][1])[0]
-                    dz=np.diff(grid[i_ang,i_dang][2])[0]
-                    X,Y,Z=np.meshgrid(np.append(grid[i_ang,i_dang][0]-dx/2,grid[i_ang,i_dang][0][-1]+dx),
-                              np.append(grid[i_ang,i_dang][1]-dy/2,grid[i_ang,i_dang][1][-1]+dy),
-                              np.append(grid[i_ang,i_dang][2]-dz/2,grid[i_ang,i_dang][2][-1]+dz),indexing='ij')
-                    ax.voxels(X,Y,Z,fill,facecolor=colors[i_dang],alpha=0.1)
-                    sel_x=(points[i_ang,i_dang][0]>config['mins'][0])*(points[i_ang,i_dang][0]<config['maxs'][0])
-                    sel_y=(points[i_ang,i_dang][1]>config['mins'][1])*(points[i_ang,i_dang][1]<config['maxs'][1])
-                    sel_z=(points[i_ang,i_dang][2]>config['mins'][2])*(points[i_ang,i_dang][2]<config['maxs'][2])
-                    sel=sel_x*sel_y*sel_z
-                    plt.plot(points[i_ang,i_dang][0][sel],points[i_ang,i_dang][1][sel],points[i_ang,i_dang][2][sel],'.k',markersize=2)
-                    ax.set_aspect('equal')
-                    ax.set_xlim([config['mins'][0],config['maxs'][0]])
-                    ax.set_ylim([config['mins'][1],config['maxs'][1]])
-                    ax.set_zlim([config['mins'][2],config['maxs'][2]])
-                    ax.set_xlabel(r'$x$ [m]')
-                    ax.set_ylabel(r'$y$ [m]')
-                    ax.set_zlabel(r'$z$ [m]')
-                    dtick=np.max([np.diff(ax.get_xticks())[0],
-                                  np.diff(ax.get_yticks())[0],
-                                  np.diff(ax.get_zticks())[0]])
-                    ax.set_xticks(np.arange(config['mins'][0],config['maxs'][0]+dtick,dtick))
-                    ax.set_yticks(np.arange(config['mins'][1],config['maxs'][1]+dtick,dtick))
-                    ax.set_zticks(np.arange(config['mins'][2],config['maxs'][2]+dtick,dtick))
-                    ax.text(0,0,config['maxs'][2]/2,s=info,bbox={'edgecolor':'k','facecolor':(1,1,1,0.25)})
-
-
-def stack(array):
-    
-    return np.stack([[array[i, j] for j in range(array.shape[1])] for i in range(array.shape[0])])
+        return Output
                      
--- lisboa/lisboa/utilities.py
+++ lisboa/lisboa/utilities.py
@@ -7,6 +7,9 @@ from lisboa.logger import SingletonLogger
 import numpy as np
 from functools import wraps
 from lisboa.config import LisboaConfig
+from matplotlib import pyplot as plt
+import warnings
+warnings.filterwarnings("ignore", category=SyntaxWarning)
 
 def get_logger(
         name: str = None, verbose: bool = True, logger: Optional[object] = None, filename=None
@@ -94,3 +97,121 @@ def sphere2cart(r,azi,ele):
     return x,y,z
 
 
+def visualize_scan(Data):
+    '''
+    Visualize scan with well-sampled regions.
+    '''
+
+    #scan information
+    azi=Data.azi.values
+    ele=Data.ele.values
+    r=Data.r.values
+    info=r'$\alpha='+str(np.round(azi[0],2))+':'+str(np.round(np.diff(azi)[0],2))+':'+str(np.round(azi[-1],2))+r'^\circ$'+ '\n'+\
+         r'$\beta=' +str(np.round(ele[0],2))+':'+str(np.round(np.diff(ele)[0],2))+':'+str(np.round(ele[-1],2))+r'^\circ$' + '\n'+\
+         r'$r= '    +str(np.round(r[0],2))  +':'+str(np.round(np.diff(r)[0],2))  +':'+str(np.round(r[-1],2))+ '$ m' +'\n'+\
+         r'$\Delta n_0='+str(Data.attrs['config_Dn0'])+r'$ m'                                         + '\n'+\
+         r'$\epsilon_I='+str(np.round(Data.attrs['epsilon1'],2))+r'$'                      + '\n'+\
+         r'$\epsilon_{II}='+str(np.round(Data.attrs['epsilon2'],2))+r'$'                   + '\n'+\
+         r'$\tau_s='+str(np.round(Data.attrs['duration'],1))+r'$ s'
+     
+    coords=''
+    for c in Data.excl.coords:
+        coords+=c
+        
+    fig=plt.figure()
+    
+    #plot 2D scan
+    if coords!='xyz':
+        ax=plt.subplot(111)
+        fill=np.zeros(np.shape(Data.excl))
+        fill[Data.excl==False]=10
+        plt.pcolor(Data[coords[0]],Data[coords[1]],fill.T,cmap='Greys',vmin=0,vmax=1,alpha=0.5)
+        plt.plot(Data[coords[0]+'_points'],Data[coords[1]+'_points'],'.k',markersize=2)
+        ax.set_aspect('equal')
+        ax.set_xlim([Data.attrs['config_mins'][0],Data.attrs['config_maxs'][0]])
+        ax.set_ylim([Data.attrs['config_mins'][1],Data.attrs['config_maxs'][1]])
+        ax.set_xlabel(r'$'+str(coords[0])+'$ [m]')
+        ax.set_ylabel(r'$'+str(coords[1])+'$ [m]')
+        dtick=np.max([np.diff(ax.get_xticks())[0],
+                      np.diff(ax.get_yticks())[0]])
+        ax.set_xticks(np.arange(Data.attrs['config_mins'][0],Data.attrs['config_maxs'][0]+dtick,dtick))
+        ax.set_yticks(np.arange(Data.attrs['config_mins'][1],Data.attrs['config_maxs'][1]+dtick,dtick))
+        ax.text(0,Data.attrs['config_maxs'][1]/2,s=info,bbox={'edgecolor':'k','facecolor':(1,1,1,0.25)})
+
+    #plot 3D scan
+    elif coords=='xyz':
+        ax=plt.subplot(111,projection='3d')
+        x=Data[coords[0]].values
+        y=Data[coords[1]].values
+        z=Data[coords[2]].values
+        x_exp=Data.x_points.values
+        y_exp=Data.y_points.values
+        z_exp=Data.z_points.values
+        
+        dx=np.diff(x)[0]
+        dy=np.diff(y)[0]
+        dz=np.diff(x)[0]
+        
+        fill=Data.excl.values==False
+        X,Y,Z=np.meshgrid(np.append(x-dx/2,x[-1]+dx),np.append(y-dy/2,y[-1]+dy),np.append(z-dz/2,z[-1]+dz),indexing='ij')
+        ax.voxels(X,Y,Z,fill,facecolor='g',alpha=0.1)
+        
+        sel_x=(x_exp>Data.attrs['config_mins'][0])*(x_exp<Data.attrs['config_maxs'][0])
+        sel_y=(y_exp>Data.attrs['config_mins'][1])*(y_exp<Data.attrs['config_maxs'][1])
+        sel_z=(z_exp>Data.attrs['config_mins'][2])*(z_exp<Data.attrs['config_maxs'][2])
+        sel=sel_x*sel_y*sel_z
+        plt.plot(x_exp[sel],y_exp[sel],z_exp[sel],'.k',markersize=2)
+        ax.set_aspect('equal')
+        ax.set_xlim([Data.attrs['config_mins'][0],Data.attrs['config_maxs'][0]])
+        ax.set_ylim([Data.attrs['config_mins'][1],Data.attrs['config_maxs'][1]])
+        ax.set_zlim([Data.attrs['config_mins'][2],Data.attrs['config_maxs'][2]])
+        ax.set_xlabel(r'$x$ [m]')
+        ax.set_ylabel(r'$y$ [m]')
+        ax.set_zlabel(r'$z$ [m]')
+        dtick=np.max([np.diff(ax.get_xticks())[0],
+                      np.diff(ax.get_yticks())[0],
+                      np.diff(ax.get_zticks())[0]])
+        ax.set_xticks(np.arange(Data.attrs['config_mins'][0],Data.attrs['config_maxs'][0]+dtick,dtick))
+        ax.set_yticks(np.arange(Data.attrs['config_mins'][1],Data.attrs['config_maxs'][1]+dtick,dtick))
+        ax.set_zticks(np.arange(Data.attrs['config_mins'][2],Data.attrs['config_maxs'][2]+dtick,dtick))
+        ax.text(0,0,Data.attrs['config_maxs'][2]/2,s=info,bbox={'edgecolor':'k','facecolor':(1,1,1,0.25)})
+        
+    return fig
+
+
+def visualize_Pareto(Data):
+    
+    #extract information
+    N_ang=len(Data.index_ang)
+    N_dang=len(Data.index_dang)
+    epsilon1=Data.epsilon1.values
+    epsilon2=Data.epsilon2.values
+    azi1=Data.azi1.values
+    azi2=Data.azi2.values
+    ele1=Data.ele1.values
+    ele2=Data.ele2.values
+    dazi=Data.dazi.values
+    dele=Data.dele.values
+    
+    #plot Pareto front
+    fig=plt.figure(figsize=(18,8))
+    cmap = plt.cm.jet
+    colors = [cmap(v) for v in np.linspace(0,1,len(Data.dazi))]
+    N_row=int(np.floor(N_ang**0.5))
+    N_col=int(np.ceil(N_ang/N_row))
+    for i_ang in range(N_ang):
+        plt.subplot(N_row,N_col,i_ang+1)
+        plt.plot(epsilon1[np.arange(N_ang)!=i_ang,:],epsilon2[np.arange(N_ang)!=i_ang,:],'.k',markersize=30,alpha=0.25)
+        for i_dang in range(N_dang):
+            plt.plot(epsilon1[i_ang,i_dang],epsilon2[i_ang,i_dang],'.',
+                     color=colors[i_dang],markeredgecolor='k',markersize=30,
+                     label=r'$\Delta \alpha='+str(dazi[i_dang])+r'^\circ$, $\Delta \beta='+str(dele[i_dang])+r'^\circ$')
+        plt.xlim([0,1])
+        plt.ylim([0,1])
+        plt.xlabel(r'$\epsilon_I$')
+        plt.ylabel(r'$\epsilon_{II}$')  
+        plt.title(r'$\alpha \in ['+str(azi1[i_ang])+', '+str(azi2[i_ang])+r']^\circ$, $\beta \in ['+str(ele1[i_ang])+', '+str(ele2[i_ang])+r']^\circ$')
+        plt.grid()
+    plt.legend(draggable=True)
+    
+    return fig
